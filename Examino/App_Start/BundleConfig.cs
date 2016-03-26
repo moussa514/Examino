40d2b37dae5053ffcc5dd4130cdcf8144112acb6
Mancuniancol <scsi72@yahoo.com>
2016-03-26 06:17:03 -0400

    Semantic UI

--- Examino/App_Start/BundleConfig.cs
+++ Examino/App_Start/BundleConfig.cs
@@ -19,12 +19,12 @@ namespace Examino
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+            bundles.Add(new ScriptBundle("~/bundles/semantic").Include(
+                      "~/Scripts/semantic.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/semantic.min.css",
                       "~/Content/site.css"));
         }
     }
